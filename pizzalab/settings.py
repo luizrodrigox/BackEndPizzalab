7b18f16197c321240984b58831f64c89800d6500
Luiz Rodrigo <melo9657@gmail.com>
2026-04-10 16:20:18 -0300

    Configurações do BD

--- pizzalab/settings.py
+++ pizzalab/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'pizzalab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nome do banco',
+        'USER': 'postgres',
+        'PASSWORD': 'senha de acesso ao banco',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

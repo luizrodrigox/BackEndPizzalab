5dba19847da6aa3ff035a647e8d050fb1b996736
Luiz Rodrigo <melo9657@gmail.com>
2026-05-05 13:27:01 -0300

    Atualizando informações gerais

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,14 +7,49 @@ def home(request):
 
     return JsonResponse({
         "sistema": "Pizzalab",
-        "versao": "1.0",
+        "versao": "3.0",
         "status": "online",
         "descricao": "Sistema web para gestão de pedidos, clientes, produção e cardápio",
+
         "rotas disponiveis": {
-            "admin":"/admin/",
-            "cardapio": "/cardapio/",
-            "clientes": "/clientes",
-            "pedidos": "/pedidos/",
-            "producao": "/producao/",
-        }
+
+            "auth": {
+                "register": "/auth/register/",
+                "login": "/auth/login/",
+            },
+
+            "clientes": {
+                "listar": "/clientes/",
+                "criar": "/clientes/criar/",
+                "detalhe": "/clientes/<id>/",
+                "atualizar": "/clientes/atualizar/<id>/",
+                "deletar": "/clientes/deletar/<id>/",
+            },
+
+            "cardapio": {
+                "listar": "/cardapio/",
+                "criar": "/cardapio/criar/",
+                "detalhe": "/cardapio/<id>/",
+                "atualizar": "/cardapio/atualizar/<id>/",
+                "deletar": "/cardapio/deletar/<id>/",
+            },
+
+            "pedidos": {
+                "listar": "/pedidos/",
+                "criar": "/pedidos/criar/",
+                "detalhe": "/pedidos/<id>/",
+                "atualizar": "/pedidos/atualizar/<id>/",
+                "deletar": "/pedidos/deletar/<id>/",
+            },
+
+            "producao": {
+                "listar": "/producao/",
+                "criar": "/producao/criar/",
+                "detalhe": "/producao/<id>/",
+                "atualizar": "/producao/atualizar/<id>/",
+                "deletar": "/producao/deletar/<id>/",
+            },
+        },
+
+        "observacao": "Rotas (exceto auth, /cardapio/ e /cardapio/<id>) requerem JWT no header: Authorization: Bearer <token>",
     })
